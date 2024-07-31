ff55fd7b8eb422ce40d038c71c34b9869d503076
Nathan Lam <nathanl1605@gmail.com>
2024-07-31 14:11:40 +0100

    Added an infinite loop to display the options until user decides to exit

--- Main.java
+++ Main.java
@@ -8,12 +8,53 @@ public class Main{
 
 
         System.out.println("----------[ WELCOME TO SIMPLE BANK ]----------\n");
-        for(int i = 0; i < options.length; i++){
-            System.out.println((i + 1) + ". " + options[i]);
+
+
+        while(true){
+            for(int i = 0; i < options.length; i++){
+                System.out.println((i + 1) + ". " + options[i]);
+            }
+    
+            System.out.print("\nPlease select an option (1-5) : ");
+            userInput = input.nextInt();
+            System.out.print("\n");
+    
+    
+            switch(userInput){
+                case 1:
+                    createAccount();
+                    break;
+                case 2:
+                    deposit();
+                    break;
+                case 3:
+                    withdraw();
+                    break;
+                case 4:
+                    checkBal();
+                    break;
+                case 5:
+                    System.out.println("Exiting....");
+                    input.close();
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Invalid Option. Please select a valid option (1 - 5)");
+    
+            }
+
         }
-
-        System.out.print("\nPlease select an option (1-5) : ");
-        userInput = input.nextInt();
-
+    }
+    public static void createAccount(){
+        System.out.println("Created");
+    }
+    public static void deposit(){
+        System.out.println("deposit");
+    }
+    public static void withdraw(){
+        System.out.println("withdraw");
+    }
+    public static void checkBal(){
+        System.out.print("balance");
     }
 }
